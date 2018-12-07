22d6093189e824abf0f3773332d4ca6f493e6862
Gerhard Pretorius <gerhardcit@gmail.com>
2018-12-07 16:43:41 +0200

    use mongo connetion string from environment

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-var express = require('express');
+const express = require('express');
+const mongodb = require('mongodb');
 
 var app = express();
 
 app.get('/', function (req, res) {
- // res.send('Hello abhilash branch World!');
- 
- 
-const mongodb = require('mongodb');
 
-var MongoClient = require('mongodb').MongoClient;
-var uri = "YOUR_URL";
-MongoClient.connect(uri, function (err, db) {
-db.close();
-if (err) {
-res.send(err);
-} else {
-res.send("Connected success");
-}
-});
+    var MongoClient = mongodb.MongoClient;
+    var uri = process.env.MONGO_DB_CONNECTION;
+    MongoClient.connect(uri, function (err, db) {
+        db.close();
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Connected success");
+        }
+    });
 
- 
 });
 
 var port = process.env.PORT || 1337;
-var server = app.listen(port,function(){
-	
+var server = app.listen(port, function () {
+
 });
 
 console.log("Server running at http://localhost:%d", port);
